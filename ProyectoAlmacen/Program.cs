caefa2e3de75579b1dedbeea8c7ea795d630e0d7
Victor Mendoza <lopezhoracio279@gmail.com>
2026-02-09 19:56:09 -0600

    Commit inicial del Proyeto de inventario

--- ProyectoAlmacen/Program.cs
+++ ProyectoAlmacen/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace ProyectoAlmacen
 {
+    //Inicio de el sistema de control de inventario
+
+
     internal class Program
     {
         static void Main(string[] args)
