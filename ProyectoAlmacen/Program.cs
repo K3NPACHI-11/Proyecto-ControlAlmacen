5534cf61b4ecd6e84369cc29d135f50a3f995baa
Samir <samirprueba@gmail.com>
2026-02-15 20:19:31 -0600

    Creacion del menu principal

--- ProyectoAlmacen/Program.cs
+++ ProyectoAlmacen/Program.cs
@@ -8,8 +8,54 @@ namespace ProyectoAlmacen
 {
     internal class Program
     {
+        static Inventario inventario = new Inventario();
+
         static void Main(string[] args)
         {
+            bool salir = false; 
+
+            while (!salir) 
+            {
+                MostrarMenu();
+                string opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        AgregarElectronico();
+                        break;
+
+                    case "2":
+                        AgregarAlimenticio();
+                        break;
+
+                    case "3":
+                        inventario.ListarProductos();
+                        break;
+
+                    case "4":
+                        EliminarProducto();
+                        break;
+
+                    case "5":
+                        salir = true;
+                        Console.WriteLine("Saliendo del sistema...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opción no válida. Elija del 1 al 5");
+                }
+            }
+        }
+
+        static void MostrarMenu()
+        {
+            Console.WriteLine("Sistema de inventario:");
+            Console.WriteLine("1. Agregar producto electrónico");
+            Console.WriteLine("2. Agregar producto alimenticio");
+            Console.WriteLine("3. Listar productos.");
+            Console.WriteLine("4. Eliminar Producto");
+            Console.WriteLine("5. Salir");
         }
     }
 }
