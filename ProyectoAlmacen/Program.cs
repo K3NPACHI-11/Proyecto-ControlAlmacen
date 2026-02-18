0e9b947d480782640d4b905f286faf9b8d97613a
Samir <samirprueba@gmail.com>
2026-02-17 20:17:10 -0600

    Directorios

--- ProyectoAlmacen/Program.cs
+++ ProyectoAlmacen/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProyectoAlmacen.Models;
+using ProyectoAlmacen.Services;
 
 namespace ProyectoAlmacen
 {
